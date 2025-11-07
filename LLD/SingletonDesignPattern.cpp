528d7760551edf814b78d7d08c68f71d0fb5bf0f
Harpreet-Narwal <harpreetnarwal9@gmail.com>
2025-11-07 22:30:40 +0530

    Singleton Design pattern

--- LLD/SingletonDesignPattern.cpp
+++ LLD/SingletonDesignPattern.cpp
@@ -24,3 +24,23 @@ public:
 };
 
 EagerSingleton EagerSingleton::instance;
+
+class LazySingleton{
+private:
+    static LazySingleton* instance;
+
+    LazySingleton(){
+
+    }
+
+public:
+    LazySingleton(const LazySingleton&) = delete;
+    LazySingleton& operator = (const LazySingleton&) = delete;
+
+    static LazySingleton* getInstance(){
+        if(instance == nullptr){
+            instance = new LazySingleton();
+        }
+        return instance;
+    }
+};
