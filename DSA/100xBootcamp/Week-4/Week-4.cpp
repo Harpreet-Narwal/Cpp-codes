4a5aa19b65b4058a8dbe2d0cb46c3951bd5b24af
Harpreet-Narwal <harpreetnarwal9@gmail.com>
2026-02-06 23:10:46 +0530

    Week-4 Array, 1st question

---
+++ DSA/100xBootcamp/Week-4/Week-4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// Question A: Reverse the Array:
+
+// int main(){
+
+//     int n;
+//     cin>>n;
+//     vector<int> arr(n);
+//     for(int i=0; i<n; i++){
+//         cin>>arr[i];
+//     }
+
+//     for(int i=n-1; i>= 0; i--){
+//         cout<<arr[i]<<" ";
+//     }
+// }
+
+
