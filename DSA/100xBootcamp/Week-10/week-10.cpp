6dc681a1e98fc3c7e6245e72151891bf1bcbd27e
Harpreet-Narwal <harpreetnarwal9@gmail.com>
2026-03-24 23:03:43 +0530

    Week-10

--- DSA/100xBootcamp/Week-10/week-10.cpp
+++ DSA/100xBootcamp/Week-10/week-10.cpp
@@ -57,87 +57,192 @@ using namespace std;
 
 // C: Count Distinct Queries:
 
-int main(){
-    int n;
-    cin>>n;
+// int main(){
+//     int n;
+//     cin>>n;
 
-    unordered_map<int, int> mpp;
+//     unordered_map<int, int> mpp;
 
-    while(n--){
-        int n;
-        cin>>n;
+//     while(n--){
+//         int n;
+//         cin>>n;
 
-        if(n == 1){
-            int x;
-            cin>>x;
-            mpp[x]++;
-        }
+//         if(n == 1){
+//             int x;
+//             cin>>x;
+//             mpp[x]++;
+//         }
 
-        if(n==2){
-            int x;
-            cin>>x;
-            mpp.erase(x);
-        }
+//         if(n==2){
+//             int x;
+//             cin>>x;
+//             mpp.erase(x);
+//         }
 
-        if(n==3){
-            cout<<mpp.size()<<"\n";
-        }
+//         if(n==3){
+//             cout<<mpp.size()<<"\n";
+//         }
 
-        if(n == 4){
-            int x;
-            cin>>x;
-            if(mpp.find(x) != mpp.end()){
-                cout<<"YES"<<"\n";
-            }else{
-                cout<<"NO"<<"\n";
-            }
-        }
+//         if(n == 4){
+//             int x;
+//             cin>>x;
+//             if(mpp.find(x) != mpp.end()){
+//                 cout<<"YES"<<"\n";
+//             }else{
+//                 cout<<"NO"<<"\n";
+//             }
+//         }
 
-    }
-}
+//     }
+// }
 
 
 // D: Count Distinct Queries-II
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     unordered_map<int, int> mpp;
+
+//     while(n--){
+//         int n;
+//         cin>>n;
+
+//         if(n == 1){
+//             int x;
+//             cin>>x;
+//             mpp[x]++;
+//         }
+
+//         if(n==2){
+//             int x;
+//             cin>>x;
+//             if(mpp.find(x) != mpp.end()){
+//                 mpp[x]--;
+//                 if(mpp[x] == 0){
+//                     mpp.erase(x);
+//                 }
+//             }
+//         }
+
+//         if(n==3){
+//             cout<<mpp.size()<<"\n";
+//         }
+
+//         if(n == 4){
+//             int x;
+//             cin>>x;
+//             if(mpp.find(x) != mpp.end()){
+//                 cout<<"YES"<<"\n";
+//             }else{
+//                 cout<<"NO"<<"\n";
+//             }
+//         }
+
+//     }
+// }
+
+
+// E: Union Of Two Arrays:
+
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     set<int> st;
+
+//     for(int i=0; i<n; i++){
+//         int x;
+//         cin>>x;
+//         st.insert(x);
+//     }
+
+//     int m;
+//     cin>>m;
+
+//     for(int i=0; i<m; i++){
+//         int x;
+//         cin>>x;
+//         st.insert(x);
+//     }
+
+//     cout<<st.size()<<endl;
+
+//     for(auto it: st){
+//         cout<<it<<" ";
+//     }
+
+// }
+
+
+// F : Intersection of Two Arrays:
+
+// int main(){
+//     int n;
+//     cin>>n;
+
+//     set<int> st;
+
+//     for(int i=0; i<n; i++){
+//         int x;
+//         cin>>x;
+//         st.insert(x);
+//     }
+
+//     int m;
+//     cin>>m;
+//     set<int> result;
+//     for(int i=0; i<m; i++){
+//         int x;
+//         cin>>x;
+//         if(st.count(x)){
+//             result.insert(x);
+//         }
+//     }
+
+//     cout<<result.size()<<"\n";
+
+//     for(auto it: result){
+//         cout<<it<<" ";
+//     }
+
+// }
+
+
+// G: Intersection of Two Array - II:
+
 int main(){
     int n;
     cin>>n;
 
-    unordered_map<int, int> mpp;
+    map<int, int> mppA;
 
-    while(n--){
-        int n;
-        cin>>n;
+    for(int i=0; i<n; i++){
+        int x; cin>>x;
+        mppA[x]++;
+    }
 
-        if(n == 1){
-            int x;
-            cin>>x;
-            mpp[x]++;
+    int m;
+    cin>>m;
+
+    map<int, int> mppB;
+    for(int i=0; i<m; i++){
+        int x;
+        cin>>x;
+        mppB[x]++;
+    }
+
+    vector<int> result;
+    for(auto it: mppA){
+        int val = it.first;
+        int cnt = min(mppA[val], mppB[val]);
+        for(int i=0; i<cnt; i++){
+            result.push_back(val);
         }
+    }
 
-        if(n==2){
-            int x;
-            cin>>x;
-            if(mpp.find(x) != mpp.end()){
-                mpp[x]--;
-                if(mpp[x] == 0){
-                    mpp.erase(x);
-                }
-            }
-        }
-
-        if(n==3){
-            cout<<mpp.size()<<"\n";
-        }
-
-        if(n == 4){
-            int x;
-            cin>>x;
-            if(mpp.find(x) != mpp.end()){
-                cout<<"YES"<<"\n";
-            }else{
-                cout<<"NO"<<"\n";
-            }
-        }
-
+    cout<<result.size()<<"\n";
+    for(int x : result){
+        cout<<x<<" ";
     }
 }
