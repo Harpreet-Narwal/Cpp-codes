43f708ab97ca0b8fe1ce5bc8d141c7c67116e59d
Harpreet-Narwal <harpreetnarwal9@gmail.com>
2026-04-23 23:51:15 +0530

    C & D problems

--- DSA/100xBootcamp/Week-11/week-11.cpp
+++ DSA/100xBootcamp/Week-11/week-11.cpp
@@ -38,40 +38,110 @@ typedef long long ll;
 
 // B : Count Vowels:
 
-bool containsVowels(char s){
-    char c = tolower(s);
+// bool containsVowels(char s){
+//     char c = tolower(s);
 
-    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
-        return true;
-    }
-    return false;
-}
+//     if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+//         return true;
+//     }
+//     return false;
+// }
+
+
+// int main(){
+//     ll n, k;
+//     cin>>n>>k;
+
+//     string s;
+//     cin>>s;
+
+//     int cntOfVowels = 0;
+
+//     for(int i=0; i<k; i++){
+//         if(containsVowels(s[i])){
+//             cntOfVowels++;
+//         }
+//     }
+
+//     cout<<cntOfVowels;
+
+//     for(int i=k; i<n; i++){
+//         if(containsVowels(s[i])) cntOfVowels++;
+//         if(containsVowels(s[i-k])) cntOfVowels--;
+
+//         cout<<" "<<cntOfVowels;
+//     }
+
+//     cout<<"\n";
+
+// }
+
+
+// C : Count Number of Distinct Integer:
+
+
+// int main(){
+//     ll n, k;
+//     cin>>n>>k;
+
+//     vector<ll> arr(n);
+//     for(int i=0; i<n; i++){
+//         cin>>arr[i];
+//     }
+
+//     unordered_map<ll, ll> mpp;
+
+//     for(int i=0; i<k; i++){
+//         mpp[arr[i]]++;
+//     }
+
+//     cout<<mpp.size();
+
+//     for(int i=k; i<n; i++){
+//         mpp[arr[i]]++;
+//         mpp[arr[i-k]]--;
+
+//         if(mpp[arr[i-k]] == 0) mpp.erase(arr[i-k]);
+
+//         cout<<" "<<mpp.size();
+//     }
+//     cout<<"\n";
+
+// }
+
+
+// D : Count Good Numbers:
 
 
 int main(){
-    ll n, k;
-    cin>>n>>k;
+    ll n, m, k;
+    cin>>n>>m>>k;
 
-    string s;
-    cin>>s;
+    vector<ll> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
 
-    int cntOfVowels = 0;
+    unordered_set<ll> goodNumbers;
+    for(int i=0; i<m; i++){
+        ll x; cin>>x;
+        goodNumbers.insert(x);
+    }
+
+    int cnt = 0;
 
     for(int i=0; i<k; i++){
-        if(containsVowels(s[i])){
-            cntOfVowels++;
-        }
+        if(goodNumbers.count(arr[i])) cnt++;
     }
 
-    cout<<cntOfVowels;
+    cout<<cnt;
 
     for(int i=k; i<n; i++){
-        if(containsVowels(s[i])) cntOfVowels++;
-        if(containsVowels(s[i-k])) cntOfVowels--;
+        if(goodNumbers.count(arr[i])) cnt++;
+        if(goodNumbers.count(arr[i-k])) cnt--;
 
-        cout<<" "<<cntOfVowels;
+        cout<<" " << cnt;
     }
-
     cout<<"\n";
-
-}
+    return 0;
+}   
